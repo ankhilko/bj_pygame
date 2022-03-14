a24efb3869e29ba7a549cc6ca8bf72e057d5a509
ankhilko <fieldstreet.27@gmail.com>
2022-03-15 01:31:58 +0300

    created the GameObject class

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import pygame
+
+
+class GameObject:
+    def __init__(self, x_y, size=1):
+        self.x_y = x_y
+        self.size = size
+        pass
+    pass
+
+
+class Card(GameObject):
+    def __init__(self):
+        super().__init__()
+        pass
+    pass
+
+
+
+
+
+
