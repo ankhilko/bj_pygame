07635bc98e738675dda3157a9806632414e8ed99
ankhilko <fieldstreet.27@gmail.com>
2022-03-17 00:28:36 +0300

    wrote the deck in text

--- main.py
+++ main.py
@@ -2,18 +2,51 @@ import random
 import pygame
 
 
+card_ranks = {str(i): i for i in range(2, 11)}
+for i in ('J', 'Q', 'K'):
+    card_ranks[i] = 10
+card_ranks['A'] = 11
+card_ranks['a'] = 1
+card_suits = ['clubs', 'diamonds', 'hearts', 'spades']
+
+deck = [(i, j) for j in card_suits for i in card_ranks if i != 'a']
+
+
 class GameObject(object):
-    def __init__(self, x, y):
+    """
+    a general class for all objects
+    """
+    def __init__(self, x=0, y=0):
         self.x, self.y = x, y
         pass
+
+    def draw(self, x, y):
+        pass
+
     pass
 
+
+class Deck(GameObject):
+    """return a deck of cards"""
+    def __init__(self, id):
+        super().__init__()
+        self.id = id
+        pass
+
+    pass
 
 class Card(GameObject):
-    def __init__(self, x, y):
-        super().__init__(self, x, y)
+    """
+    want to make an object Card
+    13 ranks in four suits: clubs (♧), diamonds (♢), hearts (♥) and spades (♤)
+    """
+    def __init__(self, id):
+        super().__init__()
+        self.id = id
         pass
     pass
+
+card = Card(('King', 'clubs'))
 
 
 
