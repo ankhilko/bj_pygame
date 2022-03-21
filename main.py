1b28180a896703fcc0b9dfb1f8973c114deed2d5
ankhilko <fieldstreet.27@gmail.com>
2022-03-22 00:32:06 +0300

    added the deck creation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 import pygame
 
-
 card_ranks = {str(i): i for i in range(2, 11)}
 for i in ('J', 'Q', 'K'):
     card_ranks[i] = 10
@@ -16,6 +15,7 @@ class GameObject(object):
     """
     a general class for all objects
     """
+
     def __init__(self, x=0, y=0):
         self.x, self.y = x, y
         pass
@@ -28,31 +28,39 @@ class GameObject(object):
 
 class Deck(GameObject):
     """return a deck of cards"""
-    def __init__(self, id):
+    global deck
+
+    def __init__(self):
         super().__init__()
-        self.id = id
+        self.deck = []
+        self.create_new_deck(deck)
+        pass
+
+    def create_new_deck(self, card_list):
+        self.deck = []
+        for card in card_list:
+            self.deck.append(Card(card))
+        random.shuffle(self.deck)
         pass
 
     pass
+
 
 class Card(GameObject):
     """
     want to make an object Card
     13 ranks in four suits: clubs (♧), diamonds (♢), hearts (♥) and spades (♤)
     """
+
     def __init__(self, id):
         super().__init__()
         self.id = id
         pass
+
     pass
 
-card = Card(('King', 'clubs'))
 
-# A lot of work is needed
-
-
-asdfghjkl;'
-
+new_deck = Deck().deck
 
 
 
