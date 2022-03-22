4a51bb72cc88470574d06675fc3cd556ac3f4b0f
ankhilko <fieldstreet.27@gmail.com>
2022-03-22 21:25:57 +0300

    started to add some drawings...

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import random
 import pygame
 
+
+W_SIZE = [800, 600]
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 card_ranks = {str(i): i for i in range(2, 11)}
 for i in ('J', 'Q', 'K'):
     card_ranks[i] = 10
 card_ranks['A'] = 11
 card_ranks['a'] = 1
 card_suits = ['clubs', 'diamonds', 'hearts', 'spades']
+suit_color = {
+    'clubs': RED,
+    'diamonds': BLACK,
+    'hearts': RED,
+    'spades': BLACK,
+}
 
 deck = [(i, j) for j in card_suits for i in card_ranks if i != 'a']
 
@@ -21,8 +34,9 @@ class GameObject(object):
         pass
 
     def draw(self, x, y):
+        # pygame.draw.polygon(screen, suit_color[card.id[1]], ((x, y + 100), (x + 100, y + 100), (x + 100, y), (x, y)))
+        # will make it later
         pass
-
     pass
 
 
@@ -61,6 +75,34 @@ class Card(GameObject):
 
 
 new_deck = Deck().deck
+
+for i in new_deck:
+    print(i.id)
+
+pygame.init()
+
+screen = pygame.display.set_mode(W_SIZE)
+
+pygame.display.set_caption("Black Jack in PyGame")
+
+clock = pygame.time.Clock()
+
+finish = False
+
+while not finish:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+
+    screen.fill(WHITE)
+
+    # refresh rate
+    pygame.time.Clock().tick(60)
+    pygame.display.flip()
+
+
+
 
 
 
